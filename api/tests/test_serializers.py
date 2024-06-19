97e8863c655809829b872784cc32b51c1a5a116f
Nicolas Arnouts <arnouts.software@gmail.com>
2024-06-20 00:43:59 +0200

    CharacterBasicSerializerTest & CharacterFullSerializerTest

--- api/tests/test_serializers.py
+++ api/tests/test_serializers.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from api.models import Character, Team
-from api.serializers import CharacterSerializer, TeamSerializer
+from api.serializers import (
+    CharacterBasicSerializer,
+    CharacterFullSerializer,
+    TeamSerializer,
+)
 
 
-class CharacterSerializerTest(TestCase):
+class CharacterBasicSerializerTest(TestCase):
     def setUp(self):
         self.character_attributes = {
             "id": 1,
@@ -13,7 +17,7 @@ class CharacterSerializerTest(TestCase):
             "affiliations": [],
         }
         self.character = Character.objects.create(**self.character_attributes)
-        self.serializer = CharacterSerializer(instance=self.character)
+        self.serializer = CharacterBasicSerializer(instance=self.character)
 
     def test_contains_expected_fields(self):
         data = self.serializer.data
@@ -25,6 +29,116 @@ class CharacterSerializerTest(TestCase):
     def test_name_field_content(self):
         data = self.serializer.data
         self.assertEqual(data["name"], self.character_attributes["name"])
+
+    def test_height_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data["height"], self.character_attributes["height"])
+
+    def test_mass_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data["mass"], self.character_attributes["mass"])
+
+
+class CharacterFullSerializerTest(TestCase):
+    def setUp(self):
+        self.character_attributes = {
+            "id": 1,
+            "name": "Luke Skywalker",
+            "height": 172,
+            "mass": 77,
+            "gender": "male",
+            "homeworld": "Tatooine",
+            "wiki": "http://starwars.wikia.com/wiki/Luke_Skywalker",
+            "image": "http://starwars.wikia.com/wiki/Luke_Skywalker/image.jpg",
+            "born": "19BBY",
+            "bornLocation": "Polis Massa",
+            "died": None,
+            "diedLocation": None,
+            "species": "Human",
+            "hairColor": "Blond",
+            "eyeColor": "Blue",
+            "skinColor": "Light",
+            "cybernetics": "",
+            "affiliations": [],
+            "masters": [],
+            "apprentices": [],
+            "formerAffiliations": [],
+            "creator": "George Lucas",
+            "manufacturer": "",
+            "model": "",
+            "class_field": "",
+            "sensorColor": "",
+            "platingColor": "",
+            "equipment": [],
+            "era": [],
+            "kajidic": "",
+            "armament": [],
+        }
+        self.character = Character.objects.create(**self.character_attributes)
+        self.serializer = CharacterFullSerializer(instance=self.character)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(
+            set(data.keys()),
+            set(
+                [
+                    "id",
+                    "name",
+                    "height",
+                    "mass",
+                    "gender",
+                    "homeworld",
+                    "wiki",
+                    "image",
+                    "born",
+                    "bornLocation",
+                    "died",
+                    "diedLocation",
+                    "species",
+                    "hairColor",
+                    "eyeColor",
+                    "skinColor",
+                    "cybernetics",
+                    "affiliations",
+                    "masters",
+                    "apprentices",
+                    "formerAffiliations",
+                    "creator",
+                    "manufacturer",
+                    "model",
+                    "class_field",
+                    "sensorColor",
+                    "platingColor",
+                    "equipment",
+                    "era",
+                    "kajidic",
+                    "armament",
+                ]
+            ),
+        )
+
+    def test_name_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data["name"], self.character_attributes["name"])
+
+    def test_gender_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data["gender"], self.character_attributes["gender"])
+
+    def test_species_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data["species"], self.character_attributes["species"])
+
+    def test_born_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(data["born"], self.character_attributes["born"])
+
+    def test_affiliations_field_content(self):
+        data = self.serializer.data
+        self.assertEqual(
+            data["affiliations"], self.character_attributes["affiliations"]
+        )
 
 
 class TeamSerializerTest(TestCase):
